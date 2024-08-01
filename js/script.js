7ac5889cff92086c8da51a46d36a2d0401cb2ff2
Ritu Anuragi <81502161+rituanuragi@users.noreply.github.com>
2024-08-01 12:20:24 +0530

    changes in functionality

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-function sendMailGetLoan(){
-    let parms = {
-        getloan_name: document.getElementById("getLoanFormName").value,
-        getloan_email: document.getElementById("getLoanFormEmail").value,
-        getloan_contact: document.getElementById("getLoanFormContact").value,
-    }
+function sendMailGetLoan() {
+  let parms = {
+    getloan_name: document.getElementById("getLoanFormName").value,
+    getloan_email: document.getElementById("getLoanFormEmail").value,
+    getloan_contact: document.getElementById("getLoanFormContact").value,
+  };
 
-    emailjs.send("service_4p37zl4","template_dqg2f3d",parms).then(alert("Email has been sent"));
+  emailjs.send("service_4p37zl4", "template_dqg2f3d", parms);
 }
 
+function sendMailPopUpForm() {
+  let parms = {
+    getloan_name: document.getElementById("popUpFormName").value,
+    getloan_email: document.getElementById("popUpFormEmail").value,
+    getloan_contact: document.getElementById("popUpFormContact").value,
+  };
 
-function sendMailPopUpForm(){
-    let parms = {
-        getloan_name: document.getElementById("popUpFormName").value,
-        getloan_email: document.getElementById("popUpFormEmail").value,
-        getloan_contact: document.getElementById("popUpFormContact").value,
-    }
-
-    emailjs.send("service_4p37zl4","template_dqg2f3d",parms).then(alert("Email has been sent"));
+  emailjs.send("service_4p37zl4", "template_dqg2f3d", parms);
 }
 
-
-function sendMail(){
-    let parms = {
-        from_name: document.getElementById("contactFormName").value,
-        from_email: document.getElementById("contactFormEmail").value,
-        contactno: document.getElementById("contactFormContact").value,
-    }
-
-    emailjs.send("service_4p37zl4","template_rc1wbg9",parms).then(alert("Email has been sent"));
+function sendMail() {
+  let parms = {
+    from_name: document.getElementById("contactFormName").value,
+    from_email: document.getElementById("contactFormEmail").value,
+    contactno: document.getElementById("contactFormContact").value,
+  };
+  emailjs.send("service_4p37zl4", "template_rc1wbg9", parms).then(() => {
+    const alertMessage = alert("Email has been sent");
+    setTimeout(() => {
+      alertMessage.close();
+    }, 1000);
+  });
 }
